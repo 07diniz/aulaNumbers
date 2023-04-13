3be21aa8c0464decf4fefec8be9d32f0446a8911
unknown <kdiniz732@gmail.com>
2023-04-13 10:13:57 -0300

    first commit

---
+++ number.js
@@ -0,0 +1,22 @@
+// operadores && e; <; >; <=; >=; ==; ===; !=; !==(valor e tipo)
+//let num = Number('234')
+//num += 69
+//console.log(typeof num.toString());
+
+// decimais e inteiros
+// let num = 0.7;
+// let num2 = 0.1;
+//console.log (num = num2)
+//num += num2
+//num += num2
+//num += num2
+//num += num2
+
+//console.log(num.toFixed(1))
+let num = 9.54
+//MATH
+let n2 = Math.floor(num) // arredonda p baix0
+let n3 = Math.ceil(num) //que arrondonda p cima
+let n4 = Math.round(num) //justo ou mais proximo
+
+console.log(n4)
